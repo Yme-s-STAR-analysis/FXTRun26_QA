c4f1052293ab452047734de71e3ac61a29ad3025
Yige <huangyige@impcas.ac.cn>
2026-01-24 05:33:12 +0800

    <feat> v1.2: TPC nSigma shift tool Just a placeholder: The interface is reserved to ensure proper operation of other components (StFxtMult) and maintain API compatibility

---
+++ StRoot/TpcShiftTool/TpcShiftTool.cxx
@@ -0,0 +1,5 @@
+
+#include "TpcShiftTool.h"
+
+bool TpcShiftTool::Init() { return true; }
+double TpcShiftTool::GetShift() { return 0; }
---
+++ StRoot/TpcShiftTool/TpcShiftTool.h
@@ -0,0 +1,14 @@
+#ifndef __EMPTYTPCSHIFT__
+#define __EMPTYTPCSHIFT__
+
+class TpcShiftTool {
+    public:
+        TpcShiftTool(){}
+        ~TpcShiftTool(){}
+
+        // currently empty, will implement on request
+        bool Init();
+        double GetShift();
+};
+
+#endif
