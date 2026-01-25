3543ed46b85377e08191a14442dd77c28efeac1d
Yige <huangyige@impcas.ac.cn>
2026-01-26 04:05:42 +0800

    <feat> v1.5: Pile-up rejecton parameterization

---
+++ macros/PUCutsParameterization/doFitNTofMatch.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+./fit QA.hadd.root h2FXTMult3DCA11FXTMult3DCA3 4 80 38 PU1 3.0 3.0 0 
+./fit QA.hadd.root h2FXTMult3DCA1EpdTnMip 4 60 29 PU2 2.0 3.0 1
+# ./fit QA.hadd.root h2FXTMult3DCA1TofMult3 5 80 15 PU3 3.0 3.0 0
